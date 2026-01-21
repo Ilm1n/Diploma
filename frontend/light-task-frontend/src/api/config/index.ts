a1f27131634d912a934237f382cec39ad6dbe0db
Ilm1n <mininiv2005@gmail.com>
2026-01-21 21:25:17 +0300

    fix url error

--- frontend/light-task-frontend/src/api/config/index.ts
+++ frontend/light-task-frontend/src/api/config/index.ts
@@ -1,7 +1,9 @@
 import {LightTaskClient} from '../client';
 import axios from 'axios';
 
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+export const API_BASE_URL = import.meta.env.VITE_API_URL !== undefined
+  ? import.meta.env.VITE_API_URL
+  : 'http://localhost:8000';
 
 export const apiClient = new LightTaskClient({
   BASE: API_BASE_URL,
