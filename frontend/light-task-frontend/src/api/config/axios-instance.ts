62b9bd825f187f75144edb6c49a2433226fa053e
Ilm1n <mininiv2005@gmail.com>
2026-01-27 04:30:23 +0300

    fix front build

--- frontend/light-task-frontend/src/api/config/axios-instance.ts
+++ frontend/light-task-frontend/src/api/config/axios-instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
 
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+export const API_BASE_URL = import.meta.env.PROD ? '' : 'http://localhost:8000';
 
 export const apiInstance: AxiosInstance = axios.create({
   baseURL: API_BASE_URL,
