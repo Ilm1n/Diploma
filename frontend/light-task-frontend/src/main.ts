645d04e74e8a34a77c8e21632c3bc9affde18603
Ilm1n <mininiv2005@gmail.com>
2026-03-02 06:08:54 +0300

    optimize fonts and assets

--- frontend/light-task-frontend/src/main.ts
+++ frontend/light-task-frontend/src/main.ts
@@ -8,13 +8,6 @@ import Aura from '@primevue/themes/aura';
 import ToastService from 'primevue/toastservice';
 import ConfirmationService from 'primevue/confirmationservice';
 
-import '@fontsource/montserrat/300.css';
-import '@fontsource/montserrat/400.css';
-import '@fontsource/montserrat/500.css';
-import '@fontsource/montserrat/600.css';
-import '@fontsource/montserrat/700.css';
-
-import 'primeicons/primeicons.css';
 import './style.css';
 import '@/assets/styles/primevue-overrides.css';
 import {definePreset} from "@primevue/themes";
